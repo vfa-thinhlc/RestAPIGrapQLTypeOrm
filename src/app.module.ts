f981fa9c0f81d3a5da4138696c76b1ee3719a052
vfa-thinhlc <vfa.internship.thinhlc@gmail.com>
2022-07-12 20:58:59 +0700

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthorModule } from './author/author.module';
+
+@Module({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+  }), BookModule, AuthorModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
